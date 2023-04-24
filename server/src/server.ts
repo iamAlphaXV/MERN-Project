b58faca2b10bf89dc537500f3491b667ffae193e
Marques Carter <KingCarterXV@gmail.com>
2023-04-24 16:06:04 -0400

    Server Env Built

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
-import express from 'express'
-const app = express();
-const port = 5000;
+import app from './app'
+import env from "./util/validateEnv";
+import mongoose from 'mongoose';
 
-app.get('/', (req,res) => {
-    res.send(`Hello, world!!!`);
-})
+const port = env.PORT
+ 
+
+mongoose.connect(env.MONGO_CONNECTION)
+.then(() => {
+console.log(`Mongoose Connected`)
 
 app.listen(port,() => {
     console.log( `server is running on port:` + port)
 })
+})
+.catch(console.error)
+
